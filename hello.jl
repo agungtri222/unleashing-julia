49f93dfadc1144b171f72e4ba275fcd4544d1663
Agung Trilaksono <agungtrilaksono123@gmail.com>
2025-01-31 06:18:26 +0000

    added pluto

--- hello.jl
+++ hello.jl
@@ -1,1 +1,23 @@
-println("Hello, World!")
+function hello(x::Int)
+    println("Hello, you are a $(x)!")
+end
+  
+function hello(x::String)
+    println("Hello, my name is $(x).")
+end
+  
+function hello(x::Bool)
+    if x
+        println("Hello, you are so honest.")
+    else
+        println("Hello, you are a liar.")
+    end
+end
+
+methods(hello)
+
+hello(10)
+hello("Mark")
+hello(false)
+hello(1.0)
+hello(x::Float64) = println("Hello, you float like $(x).")
